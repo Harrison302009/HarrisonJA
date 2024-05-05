4358f827153beb89c7941bf6f18e7644ba36fdbf
Harrison John <harrisjohnu@gmail.com>
2024-05-05 13:20:42 +0000

    Added icons and changed the navbar text style

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,17 +1,27 @@
 import { Stack } from '@mui/material';
 import '../../app/globalicons.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 export function NavBar() {
   return (
     <Stack>
       <nav id="nav">
-        <div id="home">
-          <span></span>
-          <h6>Home</h6>
+        <div className="nav-links">
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
         </div>
-        <div id="about">
-          <span></span>
-          <h6>About</h6>
+        <div id="icons">
+          <FontAwesomeIcon icon={faInstagram} id="ig" />
+          <FontAwesomeIcon icon={faFacebook} id="fb" />
+          <FontAwesomeIcon icon={faLinkedin} id="ld" />
+          <FontAwesomeIcon icon={faTwitter} id="tweet" />
         </div>
       </nav>
     </Stack>
