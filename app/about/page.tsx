b69a3e84a67c8159c8d775f8a6548abe02f5ac24
Harrison John <harrisjohnu@gmail.com>
2024-05-06 19:44:51 +0000

    Added animation and a new profile

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,7 @@
 import { Box, Stack } from '@mui/material';
 import { NavBar } from 'components/navbar/navbar';
 import '../globalicons.css';
+import Image from 'next/image';
 
 export default function HomePage() {
   return (
@@ -10,10 +11,11 @@ export default function HomePage() {
         <Box>
           <Stack
             style={{
-              backgroundImage: "url('/collage.png')",
+              backgroundImage: "url('/about-wall.jpg')",
               backgroundSize: 'cover',
               backgroundPosition: 'center',
-              height: '100vh'
+              height: '100vh',
+              overflow: 'hidden'
             }}
           >
             <NavBar />
@@ -22,7 +24,7 @@ export default function HomePage() {
                 id="text"
                 style={{
                   backgroundColor: 'rgba(255, 255, 255, 0.2)',
-                  width: '100%',
+                  width: '50%',
                   height: '100%',
                   display: 'flex',
                   position: 'relative',
@@ -32,6 +34,18 @@ export default function HomePage() {
               >
                 <h1 id="titleAbout">Harrison John-Anozie</h1>
                 <h3 id="contentAbout">Hi I&apos;m a full stack developer</h3>
+              </Stack>
+              <Stack
+                style={{ display: 'flex', position: 'relative', height: '100%', width: '50%' }}
+              >
+                <Image
+                  id="profile"
+                  alt="profile"
+                  src={'/file.png'}
+                  height={700}
+                  width={800}
+                  draggable="false"
+                ></Image>
               </Stack>
             </Stack>
           </Stack>
