ce41862365105dcf9997db2ca1741ba756bdab9b
Harrison John <harrisjohnu@gmail.com>
2024-05-09 21:45:05 +0000

    Included linkedin profile and added discord popup

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -8,8 +8,25 @@ import {
   faLinkedin,
   faTwitter
 } from '@fortawesome/free-brands-svg-icons';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 
 export function NavBar() {
+  const discordServer = () => {
+    var displayUnit = document.getElementById('grayHidden') as HTMLDivElement;
+    displayUnit.style.display = 'flex';
+  };
+  const notInterested = () => {
+    var displayUnit = document.getElementById('outputShown') as HTMLDivElement;
+    var shine = document.getElementById('grayHidden') as HTMLDivElement;
+    displayUnit.style.animation = 'alix 1s';
+    setTimeout(() => {
+      shine.style.display = 'none';
+      displayUnit.style.animation = 'dis 1s';
+    }, 600);
+  };
+  const linkProfile = () => {
+    window.location.href = 'https://www.linkedin.com/in/harrison-john-anozie-6766a7298';
+  };
   return (
     <Stack>
       <nav id="nav">
@@ -32,13 +49,20 @@ export function NavBar() {
         <div id="icons">
           <FontAwesomeIcon icon={faInstagram} id="ig" />
           <FontAwesomeIcon icon={faFacebook} id="fb" />
-          <FontAwesomeIcon icon={faLinkedin} id="ld" />
+          <FontAwesomeIcon icon={faLinkedin} id="ld" onClick={linkProfile} />
           <FontAwesomeIcon icon={faTwitter} id="tweet" />
-          <FontAwesomeIcon icon={faDiscord} id="discord" />
+          <FontAwesomeIcon icon={faDiscord} onClick={discordServer} id="discord" />
         </div>
       </nav>
       <div id="grayHidden">
-        <div id="outputShown">Join our server</div>
+        <div id="outputShown" className="outputShown">
+          <FontAwesomeIcon icon={faX} id="times" onClick={notInterested} />
+          <br />
+          <div id="inmates">
+            <FontAwesomeIcon icon={faDiscord} id="bigDiscord" />
+            <h2 id="joinTi">Join our server</h2>
+          </div>
+        </div>
       </div>
     </Stack>
   );
